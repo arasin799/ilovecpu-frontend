0f0857407dab8b18aa2f9a8dff915aea4f65e34a
arasin799 <chompoodam.ng@gmail.com>
2026-03-07 18:20:09 +0700

    fix logo size in header

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -2,9 +2,23 @@ export default function HomeHeader({ q, setQ, onSearch, cartCount = 0 }) {
   return (
     <header className="home-header">
       <div className="home-header-top">
-        <div className="home-logo">
-          <div className="home-logo-top">I LOVE</div>
-          <div className="home-logo-bottom">CPU</div>
+        <div className="home-logo-box">
+          <div className="home-logo-circuit">
+            <span className="line line-1" />
+            <span className="line line-2" />
+            <span className="line line-3" />
+            <span className="dot dot-1" />
+            <span className="dot dot-2" />
+            <span className="dot dot-3" />
+          </div>
+
+          <div className="home-logo-text">
+            <div className="logo-row-top">
+              <span className="logo-i">I</span>
+              <span className="logo-love">LOVE</span>
+            </div>
+            <div className="logo-row-bottom">CPU</div>
+          </div>
         </div>
 
         <div className="home-search">
